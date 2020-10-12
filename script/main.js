37d00f719c22c7dda55fb8bf63a0c94b087f2a1d
Amanda Barboza <amanda.brbz@hotmail.com>
2020-10-12 16:54:18 -0300

    adiciona tooltip

--- script/main.js
+++ script/main.js
@@ -3,9 +3,11 @@ import initScroll from "./modules/scroll-suave.js";
 import initAnimaAoScroll from "./modules/scroll-animacao.js";
 import initAccordion from "./modules/accordion.js";
 import initModal from "./modules/modal.js";
+import initToolTip from "./modules/tooltip.js";
 
 initTab();
 initScroll();
 initAnimaAoScroll();
 initAccordion();
 initModal();
+initToolTip();
