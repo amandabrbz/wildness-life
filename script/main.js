93e57bd0bd271bd9abddff51f55c56454efe9e81
Amanda Barboza <amanda.brbz@hotmail.com>
2020-10-12 15:18:42 -0300

    adiciona modal

--- script/main.js
+++ script/main.js
@@ -2,8 +2,10 @@ import initTab from "./modules/tab.js";
 import initScroll from "./modules/scroll-suave.js";
 import initAnimaAoScroll from "./modules/scroll-animacao.js";
 import initAccordion from "./modules/accordion.js";
+import initModal from "./modules/modal.js";
 
 initTab();
 initScroll();
 initAnimaAoScroll();
 initAccordion();
+initModal();
---
+++ script/modules/modal.js
@@ -0,0 +1,34 @@
+export default function initModal() {
+  const btnOpen = document.querySelector('[data-modal="open"]');
+  const btnClose = document.querySelector('[data-modal="close"]');
+  const container = document.querySelector('[data-modal="container"]');
+
+  if (btnOpen && btnClose && container) {
+    function openModal(event) {
+      event.preventDefault();
+      container.classList.add("active");
+    }
+
+    function closeModal(event) {
+      event.preventDefault();
+      container.classList.remove("active");
+    }
+
+    function closeEsc(event) {
+      if (event.key === 27 || event.key === "Escape") {
+        closeModal(event);
+      }
+    }
+
+    function closeFora(event) {
+      if (event.target === this) {
+        closeModal(event);
+      }
+    }
+
+    btnOpen.addEventListener("click", openModal);
+    btnClose.addEventListener("click", closeModal);
+    document.addEventListener("keydown", closeEsc);
+    container.addEventListener("click", closeFora);
+  }
+}
