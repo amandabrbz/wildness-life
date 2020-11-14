0d56b303900e3fa7e063befc673a78381cec8c68
Amanda Barboza <amanda.barboza@mirumagency.com>
2020-11-14 18:47:24 -0300

    refactor animacao ao scroll e constroi classes

--- script/modules/scroll-animacao.js
+++ script/modules/scroll-animacao.js
@@ -5,24 +5,39 @@ export default class ScrollAnima {
     this.metadeTela = window.innerHeight * 0.6;
 
     // funcoes de callback precisam de bind
-    this.animaAoScroll = this.animaAoScroll.bind(this);
+    this.checkDistance = this.checkDistance.bind(this);
   }
 
-  animaAoScroll() {
-    this.sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top - this.metadeTela;
-      const isSectionVisible = sectionTop - this.metadeTela < 0;
-      if (isSectionVisible) {
-        section.classList.add(this.classActive);
-      } else if (section.classList.contains(this.classActive)) {
-        section.classList.remove(this.classActive);
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: Math.floor(offset) - this.metadeTela,
+      };
+    });
+  }
+
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add(this.classActive);
+      } else if (item.element.classList.contains(this.classActive)) {
+        item.element.classList.remove(this.classActive);
       }
     });
   }
 
   init() {
-    this.animaAoScroll();
-    window.addEventListener("scroll", this.animaAoScroll);
+    if (this.sections.lenght) {
+      this.getDistance();
+      this.checkDistance();
+      window.addEventListener("scroll", this.checkDistance);
+    }
     return this;
+  }
+
+  stop() {
+    window.removeEventListener("scroll", this.checkDistance);
   }
 }
